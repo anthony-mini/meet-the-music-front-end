b899cc69a7464ca99ae7639dd2447dcac7231b34
anthony-mini <anthony-mini@outlook.fr>
2024-04-21 18:15:31 +0200

    wip(routes): working on routing module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from '../app/login/login.component';
+import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  { path: '', component: AppComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
