9e172d6941760c8bfe2675ed5bbdd923f06da768
anthony-mini <anthony-mini@outlook.fr>
2024-04-21 23:14:54 +0200

    wip(routes): Testing routes between different pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,18 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from '../app/login/login.component';
+import { LoginComponent } from './features/login/login.component';
 import { AppComponent } from './app.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PageNotFoundComponent } from './features/page-not-found/page-not-found.component';
+import { HomeComponent } from './features/home/home.component';
 
 export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
   {
     path: 'login',
     component: LoginComponent,
   },
-  { path: '', component: AppComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
 ];
