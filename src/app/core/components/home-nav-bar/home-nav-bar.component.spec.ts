706b93dfd822dcc75c278d18e614db0cdb91f509
anthony-mini <anthony-mini@outlook.fr>
2024-05-01 19:15:15 +0200

    update test

--- src/app/core/components/home-nav-bar/home-nav-bar.component.spec.ts
+++ src/app/core/components/home-nav-bar/home-nav-bar.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeNavBarComponent } from './home-nav-bar.component';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('HomeNavBarComponent', () => {
   let component: HomeNavBarComponent;
@@ -8,10 +9,12 @@ describe('HomeNavBarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeNavBarComponent]
-    })
-    .compileComponents();
-    
+      imports: [HomeNavBarComponent],
+      providers: [
+        { provide: ActivatedRoute, useValue: { params: of({ id: 'testId' }) } },
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(HomeNavBarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
