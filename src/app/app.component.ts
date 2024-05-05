55306696c6725753f40539810775ac41a2d51453
anthony-mini <anthony-mini@outlook.fr>
2024-05-05 22:43:59 +0200

    wip: footer and new logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeNavBarComponent } from './core/components/home-nav-bar/home-nav-bar.component';
+import { FooterComponent } from './core/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeNavBarComponent],
+  imports: [RouterOutlet, HomeNavBarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
