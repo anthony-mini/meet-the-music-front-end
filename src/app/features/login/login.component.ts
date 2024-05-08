88c756615cb0018febe376938ea8f2c710af97d1
anthony-mini <anthony-mini@outlook.fr>
2024-05-08 22:19:39 +0200

    wip: login page

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -3,10 +3,11 @@ import { FormsModule } from '@angular/forms';
 import { CreateUserDto } from 'src/app/core/models/user.dto';
 import { UserService } from 'src/app/core/services/user.service';
 import { Role } from '../../core/enums/role.enum';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
