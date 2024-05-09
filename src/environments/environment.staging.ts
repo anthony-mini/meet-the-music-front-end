35014eb481d0d3f5b52bf0a810ba70d1d5dec581
anthony-mini <anthony-mini@outlook.fr>
2024-05-09 17:42:32 +0200

    fix: add https

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -3,5 +3,5 @@
 export const environment = {
   production: false,
   meetthemusicBackendUrl:
-    'staging-back-meet-the-music-si3zs.ondigitalocean.app',
+    'https://staging-back-meet-the-music-si3zs.ondigitalocean.app',
 };
