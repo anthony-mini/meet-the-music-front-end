892ce33e3a9bb6dc143ea0b3bcee064743cecaab
anthony-mini <anthony-mini@outlook.fr>
2024-05-18 15:00:20 +0200

    chore: Update auth-interceptor service to handle expired tokens and redirect to login page

--- src/app/core/services/auth-interceptor.service.ts
+++ src/app/core/services/auth-interceptor.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
-import {
-  HttpInterceptorFn,
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-} from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpInterceptorFn } from '@angular/common/http';
+
+import { Router } from '@angular/router';
+import { inject } from '@angular/core';
 
 export const AuthInterceptor: HttpInterceptorFn = (req, next) => {
+  const router = inject(Router);
+
   const token = localStorage.getItem('access_token');
+
+  if (isTokenExpired(token)) {
+    console.log('Token expired');
+    localStorage.removeItem('access_token');
+    router.navigate(['/login']);
+    return next(req);
+  }
 
   if (token) {
     const authReq = req.clone({
@@ -20,3 +26,13 @@ export const AuthInterceptor: HttpInterceptorFn = (req, next) => {
     return next(req);
   }
 };
+
+function isTokenExpired(token: any) {
+  if (token) {
+    const expiryDate = JSON.parse(atob(token.split('.')[1])).exp;
+
+    return Math.floor(new Date().getTime() / 1000) >= expiryDate;
+  }
+
+  return true;
+}
