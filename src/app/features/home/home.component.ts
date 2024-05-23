2b9620dde59d8caf4563870d39c7003a25745738
anthony-mini <anthony-mini@outlook.fr>
2024-05-23 15:19:51 +0200

    wip: cta home page

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -3,6 +3,7 @@ import { RouterLink } from '@angular/router';
 import { BannerComponent } from '../../core/components/banner/banner.component';
 import { StatItemComponent } from 'src/app/core/components/stat-item/stat-item.component';
 import { ServiceItemComponent } from 'src/app/core/components/service-item/service-item.component';
+import { CtaConnexionComponent } from 'src/app/core/components/cta-connexion/cta-connexion.component';
 
 @Component({
   selector: 'app-home',
@@ -12,6 +13,7 @@ import { ServiceItemComponent } from 'src/app/core/components/service-item/servi
     BannerComponent,
     StatItemComponent,
     ServiceItemComponent,
+    CtaConnexionComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
