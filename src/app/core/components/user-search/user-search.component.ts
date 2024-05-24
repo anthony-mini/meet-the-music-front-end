15a08e51abf1b13e49658aca1d640cca87e53102
anthony-mini <anthony-mini@outlook.fr>
2024-05-24 16:12:34 +0200

    wip: input

--- src/app/core/components/user-search/user-search.component.ts
+++ src/app/core/components/user-search/user-search.component.ts
@@ -2,17 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { FormsModule, NgModel } from '@angular/forms';
 import { NgClass, NgFor } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user-search',
   standalone: true,
-  imports: [NgClass, FormsModule, NgFor],
+  imports: [NgClass, FormsModule, NgFor, RouterLink],
   templateUrl: './user-search.component.html',
   styleUrl: './user-search.component.scss',
 })
 export class UserSearchComponent implements OnInit {
   users: any[] = [];
   searchQuery: string = '';
+  isOpen: boolean = false;
 
   constructor(private userService: UserService) {}
 
